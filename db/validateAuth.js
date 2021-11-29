f4193010456ec9e9cf383edc67f4cb03544e2c13
JacobEckroth <jacobeckroth@gmail.com>
2021-11-29 14:29:04 -0800

    added auth checking

--- db/validateAuth.js
+++ db/validateAuth.js
@@ -1,6 +1,13 @@
-const auth = require('../auth.json');
+const fs = require('fs');
+var auth = null
 
+if(fs.existsSync('../auth.json')){
+    auth = require('../auth.json');
+}
 function validateAuth(pass){
+    if(auth == null){
+        return false;
+    }   
     if(pass !== auth.authToken){
       return false;
     }else{
